829973220c2e9463118f129dacf54362720f8bd2
Ilies Sebastian <iliessebi11@gmail.com>
2022-11-11 18:23:56 +0200

    added base colors for the ui applications

--- WowAPI/WowAPI/obj/Debug/net6.0-windows/App.g.i.cs
+++ WowAPI/WowAPI/obj/Debug/net6.0-windows/App.g.i.cs
@@ -58,7 +58,7 @@ namespace WowAPI {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/WowAPI;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WowAPI;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
